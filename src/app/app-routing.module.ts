1b8e7c9f834ef9365e8261fe27a1f9f8d75484b7
David <youchen.du@gmail.com>
2019-01-10 18:02:23 +0800

    Use /home as home page, update lint rules.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,12 @@ import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: '',
+  { path: 'home',
     component: HomeComponent,
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
   },
   { path: '**',
     component: PageNotFoundComponent,
