3c6d2e807f2124b8bf5f7a041f60154ad0d7ca31
David <youchen.du@gmail.com>
2019-01-25 17:31:32 +0800

    Fix unused import.

--- src/app/modules/roles/admin/admin.routing.ts
+++ src/app/modules/roles/admin/admin.routing.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { DashboardComponent } from 'src/app/demo/dashboard/dashboard.component';
 import { UserProfileComponent } from 'src/app/demo/user-profile/user-profile.component';
 import { TableListComponent } from 'src/app/demo/table-list/table-list.component';
 import { TypographyComponent } from 'src/app/demo/typography/typography.component';
 import { IconsComponent } from 'src/app/demo/icons/icons.component';
 import { NotificationsComponent } from 'src/app/demo/notifications/notifications.component';
 import { UpgradeComponent } from 'src/app/demo/upgrade/upgrade.component';
-import { TrainingRecordComponent } from 'src/app/modules/training-record/training-record.component';
 
 export const adminRoutes: Routes = [
   {
