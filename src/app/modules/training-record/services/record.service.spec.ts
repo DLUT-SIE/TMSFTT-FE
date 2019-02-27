6692e21370b8df31ce2cb991d67f4fa9d9bda94b
Yqzzxlj <yq736677@icloud.com>
2019-02-28 05:22:11 +0800

    fix the code style#31

--- src/app/modules/training-record/services/record.service.spec.ts
+++ src/app/modules/training-record/services/record.service.spec.ts
@@ -5,7 +5,7 @@ import { throwError, Subject } from 'rxjs';
 import { RecordService } from './record.service';
 
 import { environment } from 'src/environments/environment';
-import { RecordRequest, RecordResponse, RecordAttachmentResponse } from 'src/app/interfaces/record';
+import { RecordRequest, RecordResponse, RecordAttachmentResponse, RecordContentResponse } from 'src/app/interfaces/record';
 import { ContentType } from 'src/app/enums/content-type.enum';
 import { AUTH_SERVICE } from 'src/app/interfaces/auth-service';
 import { PaginatedResponse } from 'src/app/interfaces/paginated-response';
@@ -15,34 +15,47 @@ describe('RecordService', () => {
   let httpTestingController: HttpTestingController;
   const dummyOffCampusEvent: OffCampusEventResponse = {
     id: 1,
-    create_time: "2019-02-21T16:40:03.799178+08:00",
-    update_time: "2019-02-21T16:40:03.799197+08:00",
-    name: "一起设计继续名称.",
-    time: "2019-04-04T09:37:23.606866+08:00",
-    location: "拉萨街j座",
+    create_time: '2019-02-21T16:40:03.799178+08:00',
+    update_time: '2019-02-21T16:40:03.799197+08:00',
+    name: '一起设计继续名称.',
+    time: '2019-04-04T09:37:23.606866+08:00',
+    location: '拉萨街j座',
     num_hours: 1.9768268898717425,
     num_participants: 60
   };
   const dummyRecordAttachment: RecordAttachmentResponse = {
     id: 1,
-    create_time: "2019-02-21T16:40:11.057665+08:00",
-    update_time: "2019-02-21T16:40:11.057681+08:00",
+    create_time: '2019-02-21T16:40:11.057665+08:00',
+    update_time: '2019-02-21T16:40:11.057681+08:00',
     attachment_type: 2,
-    path: "/资料/必须/地址/都是.jpg",
+    path: '/资料/必须/地址/都是.jpg',
     record: 1
   };
   const dummyRecordAttachments: RecordAttachmentResponse[] = [
     dummyRecordAttachment,
-    dummyRecordAttachment
-  ]
+    dummyRecordAttachment,
+  ];
+  const dummyRecordContent: RecordContentResponse = {
+    id: 2,
+    create_time: '2019-02-21T16:40:07.759937+08:00',
+    update_time: '2019-02-21T16:40:07.759954+08:00',
+    content_type: 0,
+    content: '游戏积分部门精华.\n东西为什然后.留言企业选择主要大学如何.完全可能没有同时留言这个经营.\n然后影响电子成为.会员商品推荐.然后数据信息不断.全国通过来自一切分析.\n网上地址这些详细认为信息自己.',
+    record: 3
+  };
+  const dummyRecordContents: RecordContentResponse[] = [
+    dummyRecordContent,
+    dummyRecordContent,
+  ];
   const dummyRecord: RecordResponse = {
     id: 1,
-    create_time: "2019-02-21T16:40:05.769180+08:00",
-    update_time: "2019-02-21T16:40:14.231461+08:00",
+    create_time: '2019-02-21T16:40:05.769180+08:00',
+    update_time: '2019-02-21T16:40:14.231461+08:00',
     status: 3,
     campus_event: null,
     off_campus_event: dummyOffCampusEvent,
     attachments: dummyRecordAttachments,
+    contents: dummyRecordContents,
     user: 127,
   };
 
