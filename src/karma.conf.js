f56a21100771f3dc46abaec3111b2c009309c4cd
Youchen Du <youchen.du@gmail.com>
2019-03-01 15:36:32 +0800

    [Fix] Fix coverage report error.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -20,9 +20,21 @@ module.exports = function (config) {
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
+      // enforce percentage thresholds
+      // anything under these percentages will cause karma to fail with an exit code of 1 if not running in watch mode
+      thresholds: {
+        emitWarning: false, // set to `true` to not fail the test command when thresholds are not met
+        // thresholds for all files
+        global: {
+          statements: 100,
+          lines: 100,
+          branches: 100,
+          functions: 100
+        }
+      }
     },
-    reporters: ['progress', 'kjhtml', 'coverage', 'coveralls'],
+    reporters: ['progress', 'kjhtml', 'coverage-istanbul', 'coveralls'],
     coverageReporter: {
       type: 'lcov', // lcov or lcovonly are required for generating lcov.info files
       dir: '../coverage/'
