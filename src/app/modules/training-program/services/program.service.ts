8d3eeea424c109be36a6d4ea969ef2caf90ac33c
a8520238 <360238076@qq.com>
2019-03-04 12:14:12 -0800

    我增加了program组件 #32

--- src/app/modules/training-program/services/program.service.ts
+++ src/app/modules/training-program/services/program.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, Inject } from '@angular/core';
-import { timer, of as observableOf } from 'rxjs';
-import { catchError, takeWhile } from 'rxjs/operators';
+
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from 'src/environments/environment';
@@ -14,19 +13,12 @@ import { PaginatedResponse } from 'src/app/interfaces/paginated-response';
 })
 export class ProgramService {
   /** How often should we query latest programforms. */
-  private REFRESH_INTERVAL = 30 * 1000;
 
   constructor(
     private readonly http: HttpClient,
     @Inject(AUTH_SERVICE) private readonly authService: AuthService,
   ) {
     this.authService.authenticationSucceed.subscribe(() => {
-      timer(0, this.REFRESH_INTERVAL).pipe(
-        takeWhile(() => this.authService.isAuthenticated),
-        catchError(() => {
-          return observableOf(null);
-        }),
-      ).subscribe();
     });
   }
 
