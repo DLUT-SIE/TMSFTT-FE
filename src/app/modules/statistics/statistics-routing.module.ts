4538e4dac4a1b5d9a0b8e046cec75f598396cee9
Youchen Du <youchen.du@gmail.com>
2019-03-28 15:07:44 +0800

    Add placeholders for statistics module.

---
+++ src/app/modules/statistics/statistics-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from 'src/app/guards/auth.guard';
+import { StatisticsComponent } from './statistics.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { SummaryAnalysisComponent } from './components/summary-analysis/summary-analysis.component';
+import { DataExportComponent } from './components/data-export/data-export.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: StatisticsComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'summary-analysis',
+        component: SummaryAnalysisComponent,
+      },
+      {
+        path: 'data-export',
+        component: DataExportComponent,
+      },
+      {
+        path: '',
+        component: MenuComponent,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class StatisticsRoutingModule { }
