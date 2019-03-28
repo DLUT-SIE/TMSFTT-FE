f252c2056b706998d2d47ca7045d72d24fb100ae
Youchen Du <youchen.du@gmail.com>
2019-03-28 14:03:41 +0800

    Update the sidenav content.

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -5,7 +5,10 @@ import { AUTH_SERVICE, AuthService } from 'src/app/interfaces/auth-service';
 import { NotificationService } from 'src/app/modules/notification/services/notification.service';
 
 export const ADMIN_ROUTE_ITEMS: RouteInfo[] = [
+    { path: '/admin/event-management', title: '项目管理', icon: 'event', class: '' },
+    { path: '/admin/data-management', title: '数据管理', icon: 'data_usage', class: '' },
     { path: '/admin/permission-management', title: '权限管理', icon: 'security', class: '' },
+
     { path: '/demo/user-profile', title: 'Demo-User Profile', icon: 'person', class: '' },
     { path: '/demo/table-list', title: 'Demo-Table List', icon: 'content_paste', class: '' },
     { path: '/demo/typography', title: 'Demo-Typography', icon: 'library_books', class: '' },
@@ -15,10 +18,11 @@ export const ADMIN_ROUTE_ITEMS: RouteInfo[] = [
 
 export const REGULAR_USER_ROUTE_ITEMS: RouteInfo[] = [
     { path: '/dashboard', title: '首页', icon: 'dashboard', class: '' },
-    { path: '/training-record/entry', title: '培训记录填报', icon: 'create', class: '' },
-    { path: '/training-record/records', title: '培训记录列表', icon: 'list', class: '' },
-    { path: '/training-program/programs', title: '培训项目', icon: 'create', class: '' },
-    { path: '/training-event/events', title: '校内培训活动查看', icon: 'create', class: '' },
+    { path: '/training-event/events', title: '校内培训活动', icon: 'list', class: '' },
+    // TODO(youchen): Display the number of records without feedbacks.
+    { path: '/training-record/records', title: '个人培训记录', icon: 'how_to_reg', class: '' },
+    { path: '/training-record/entry', title: '校外培训填报', icon: 'create', class: '' },
+    { path: '/personal-summary', title: '个人数据统计', icon: 'unarchive', class: '' },
 ];
 
 @Component({
