b0f7ce47c01e0720a63dab408911a9e49662e281
Youchen Du <youchen.du@gmail.com>
2019-03-28 16:55:16 +0800

    Switch from chartist to ECharts

--- src/app/modules/roles/regular-user/regular-user.module.ts
+++ src/app/modules/roles/regular-user/regular-user.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule, MatFormFieldModule, MatTooltipModule } from '@angular/material';
-
+import { NgxEchartsModule } from 'ngx-echarts';
 
 import { RegularUserRoutingModule } from './regular-user.routing.module';
 import { RegularUserComponent } from './regular-user.component';
@@ -14,6 +14,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     DashboardComponent,
   ],
   imports: [
+    NgxEchartsModule,
     CommonModule,
     RouterModule,
     MatButtonModule,
