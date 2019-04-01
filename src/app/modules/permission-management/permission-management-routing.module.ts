a225bc13c3d3e3150e0e0e15473c1e518647c8d9
Youchen Du <youchen.du@gmail.com>
2019-04-01 14:38:58 +0800

    Minor updates.
    * Remove unnecessary guards.
    * Add data management module.
    * Add placeholder for department admins management route.

--- src/app/modules/permission-management/permission-management-routing.module.ts
+++ src/app/modules/permission-management/permission-management-routing.module.ts
@@ -2,20 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AccountPermissionComponent } from './components/account-permission/account-permission.component';
-import { AuthGuard } from 'src/app/guards/auth.guard';
 import { MenuComponent } from './components/menu/menu.component';
 import { PermissionManagementComponent } from './permission-management.component';
-import { AdminGuard } from 'src/app/guards/admin.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: PermissionManagementComponent,
-    canActivate: [AuthGuard],
     children: [
       {
         path: '',
-        canActivateChild: [AdminGuard],
         children: [
           {
             path: 'account-permission',
