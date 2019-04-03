490130808528ef9469dc8fa289569a87a9f62262
Yqzzxlj <yq736677@icloud.com>
2019-04-03 15:31:14 +0800

    Modify html files and urls files. close#74

--- src/app/modules/training-record/training-record-routing.module.ts
+++ src/app/modules/training-record/training-record-routing.module.ts
@@ -1,42 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecordFormComponent } from './components/record-form/record-form.component';
-import { BatchSubmitComponent } from './components/batch-submit/batch-submit.component';
-import { AuthGuard } from 'src/app/guards/auth.guard';
 import { TrainingRecordComponent } from './training-record.component';
 import { RecordListComponent } from './components/record-list/record-list.component';
 import { RecordDetailComponent } from './components/record-detail/record-detail.component';
 import { RecordDetailResolverService } from './services/record-detail-resolver.service';
 import { OffCampusEventRecordListComponent } from './components/off-campus-event-record-list/off-campus-event-record-list.component';
-import { EntryModeComponent } from './components/entry-mode/entry-mode.component';
 
 const routes: Routes = [
   {
     path: '',
     component: TrainingRecordComponent,
-    canActivate: [AuthGuard],
     children: [
       {
-        path: 'entry',
-        canActivateChild: [AuthGuard],
-        children: [
-          {
-            path: 'record-form',
-            component: RecordFormComponent,
-          },
-          {
-            path: 'batch-submit',
-            component: BatchSubmitComponent,
-          },
-          {
-            path: '',
-            component: EntryModeComponent,
-          }
-        ]
-      },
-      {
         path: 'records',
-        canActivateChild: [AuthGuard],
         children: [
           {
             path: ':id',
@@ -53,7 +30,6 @@ const routes: Routes = [
       },
       {
         path: 'off-campus-event-records',
-        canActivateChild: [AuthGuard],
         children: [
           {
             path: 'record-form',
