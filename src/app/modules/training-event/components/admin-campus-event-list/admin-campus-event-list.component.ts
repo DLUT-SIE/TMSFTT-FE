6bf3d22edeaf1cb6d8b62096ca7adfbed79c7646
dutlzn <2436013662@qq.com>
2019-04-03 14:22:57 +0800

    保存

---
+++ src/app/modules/training-event/components/admin-campus-event-list/admin-campus-event-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-campus-event-list',
+  templateUrl: './admin-campus-event-list.component.html',
+  styleUrls: ['./admin-campus-event-list.component.css']
+})
+export class AdminCampusEventListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
