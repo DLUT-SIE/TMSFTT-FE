787e267e443b8442f4fb17de4e6c81b3492491d1
Youchen Du <youchen.du@gmail.com>
2019-04-03 15:59:22 +0800

    Adapt backend changes for Notification.

--- src/app/modules/notification/services/notification.service.ts
+++ src/app/modules/notification/services/notification.service.ts
@@ -6,13 +6,14 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { NotificationResponse } from 'src/app/interfaces/notification';
 import { AUTH_SERVICE, AuthService } from 'src/app/interfaces/auth-service';
-import { PaginatedResponse } from 'src/app/interfaces/paginated-response';
+import { GenericListService } from 'src/app/generics/generic-list-service/generic-list-service';
+import { ListRequest } from 'src/app/interfaces/list-request';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class NotificationService {
+export class NotificationService extends GenericListService {
   /** Latest unread notifications. */
   unreadNotifications: NotificationResponse[] = [];
   /** Indicate whether unread notifications has been loaded. */
@@ -24,13 +25,14 @@ export class NotificationService {
   private REFRESH_INTERVAL = 30 * 1000;
 
   constructor(
-    private readonly http: HttpClient,
+    protected readonly http: HttpClient,
     @Inject(AUTH_SERVICE) private readonly authService: AuthService,
   ) {
+    super(http);
     this.authService.authenticationSucceed.subscribe(() => {
       timer(0, this.REFRESH_INTERVAL).pipe(
         takeWhile(() => this.authService.isAuthenticated),
-        switchMap(() => this.getNotifications(0, this.UNREAD_BOX_LIMIT, false)),
+        switchMap(() => this.getUnReadNotifications({offset: 0, limit: this.UNREAD_BOX_LIMIT})),
         map(res => {
           this.unreadNotifications = res.results;
           this.unreadNotificationsLoaded = true;
@@ -48,32 +50,29 @@ export class NotificationService {
       `${environment.API_URL}/notifications/${id}/`);
   }
 
-  getNotifications(offset?: number, limit?: number,
-    readStatus?: boolean) {
-    if (offset === undefined) offset = 0;
-    if (limit === undefined) limit = environment.PAGINATION_SIZE;
-    const paramsObj = { offset, limit };
-    const queryParams = Object.keys(paramsObj).map(
-      key => key + '=' + encodeURIComponent(paramsObj[key])).join('&');
-    let url = environment.API_URL + '/notifications/';
-    if (readStatus !== undefined) {
-      url += readStatus ? 'read/' : 'unread/';
-    }
-    url += '?' + queryParams;
-    return this.http.get<PaginatedResponse<NotificationResponse>>(url);
+  getNotifications(req: ListRequest) {
+    return this.list<NotificationResponse>('notifications', req);
+  }
+
+  getReadNotifications(req: ListRequest) {
+    return this.list<NotificationResponse>('notifications/read', req);
+  }
+
+  getUnReadNotifications(req: ListRequest) {
+    return this.list<NotificationResponse>('notifications/unread', req);
   }
 
   /** Mark all notifications for user as read. */
-  markAllNotificationsAsRead(userID: number) {
-    const action = 'mark-all-notifications-as-read';
-    const url = `${environment.API_URL}/users/${userID}/tasks/${action}/`;
+  markAllNotificationsAsRead() {
+    const action = 'read-all';
+    const url = `${environment.API_URL}/notifications/actions/${action}/`;
     return this.http.post(url, {});
   }
 
   /** Delete all notifications for user. */
-  deleteAllNotifications(userID: number) {
-    const action = 'delete-all-notifications';
-    const url = `${environment.API_URL}/users/${userID}/tasks/${action}/`;
+  deleteAllNotifications() {
+    const action = 'delete-all';
+    const url = `${environment.API_URL}/notifications/actions/${action}/`;
     return this.http.post(url, {});
   }
 
