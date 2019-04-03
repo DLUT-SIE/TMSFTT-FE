2c0d871a36f77d7c920feae070ab96cf450c5916
dutlzn <2436013662@qq.com>
2019-04-03 20:52:25 +0800

    #78
    保存

--- src/app/modules/training-event/components/admin-campus-event-list/admin-campus-event-list.component.ts
+++ src/app/modules/training-event/components/admin-campus-event-list/admin-campus-event-list.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { CampusEventResponse } from 'src/app/interfaces/event';
+import { EventService  } from '../../services/event.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { GenericListComponent } from 'src/app/generics/generic-list/generic-list';
 
 @Component({
   selector: 'app-admin-campus-event-list',
   templateUrl: './admin-campus-event-list.component.html',
   styleUrls: ['./admin-campus-event-list.component.css']
 })
-export class AdminCampusEventListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class AdminCampusEventListComponent extends GenericListComponent<CampusEventResponse> implements OnInit {
+  programId: number;
+  constructor(
+    private readonly eventService: EventService,
+    protected readonly route: ActivatedRoute,
+    protected readonly router: Router,
+  ) {
+    super(route, router);
   }
 
+  ngOnInit() {
+    this.route.queryParams.subscribe(queryParams => {
+      this.programId = queryParams.program_id;
+  });
+  }
+
+  getResults(offset: number, limit: number) {
+    let extraParams = new Map();
+    extraParams.set('program',this.programId)
+    return this.eventService.getCampusEvents({offset, limit, extraParams});
+  }
 }
