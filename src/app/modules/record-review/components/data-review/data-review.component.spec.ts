f25261daf7c5323ca617d8815dc81be7b815c380
ghh82058 <guanhuih@outlook.com>
2019-04-04 17:29:43 +0800

    add an area to allow admins creating review notes and viewing feedbacks from users.Close #81.

--- src/app/modules/record-review/components/data-review/data-review.component.spec.ts
+++ src/app/modules/record-review/components/data-review/data-review.component.spec.ts
@@ -1,21 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { of as observableOf } from 'rxjs';
-import { MatCardModule, MatIconModule } from '@angular/material';
-import { ActivatedRoute } from '@angular/router';
+import { of as observableOf, Subject } from 'rxjs';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatCardModule,
+  MatPaginatorModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { DataReviewComponent } from './data-review.component';
+import { ReviewNoteService } from '../../services/review-note.service';
+import { ReviewNoteResponse } from 'src/app/interfaces/review-note';
+import { PaginatedResponse } from 'src/app/interfaces/paginated-response';
 import { RecordResponse } from 'src/app/interfaces/record';
 
 describe('DataReviewComponent', () => {
   let component: DataReviewComponent;
   let fixture: ComponentFixture<DataReviewComponent>;
+  let getReviewNotes$: Subject<PaginatedResponse<ReviewNoteResponse>>;
+  const dummyReviewNote: ReviewNoteResponse = {
+    "id": 1,
+    "create_time": "2019-02-23T20:37:57.127073+08:00",
+    "field_name": "time",
+    "content": "组织自己电子国内控制一次登录这样能够",
+    "record": 2,
+    "user": 48,
+  }
 
   beforeEach(async(() => {
+    getReviewNotes$ = new Subject<PaginatedResponse<ReviewNoteResponse>>();
     TestBed.configureTestingModule({
       declarations: [ DataReviewComponent ],
       imports: [
         MatCardModule,
+        MatPaginatorModule,
         MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        NoopAnimationsModule
       ],
       providers: [
         {
@@ -40,8 +64,18 @@ describe('DataReviewComponent', () => {
               user: 1,
               status: 1,
             } as RecordResponse}),
-          },
-        }
+          }
+        },
+        {
+          provide: Router,
+          useValue: {}
+        },
+        {
+          provide: ReviewNoteService,
+          useValue: {
+            getReviewNotes: () => getReviewNotes$,
+          }
+        },
       ]
     })
     .compileComponents();
@@ -56,4 +90,27 @@ describe('DataReviewComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load data', () => {
+    const results: ReviewNoteResponse[] = [dummyReviewNote, dummyReviewNote];
+    getReviewNotes$.next({
+        count: 100,
+        results,
+        next: '',
+        previous: ''
+    });
+
+    expect(component.isLoadingResults).toBeFalsy();
+    expect(component.results).toEqual(results);
+    expect(component.resultsLength).toEqual(100);
+  });
+
+  it('should empty data if an error encountered.', () => {
+    getReviewNotes$.error('a');
+
+    expect(component.isLoadingResults).toBeFalsy();
+    expect(component.results).toEqual([]);
+    expect(component.resultsLength).toEqual(0);
+  });
+
 });
