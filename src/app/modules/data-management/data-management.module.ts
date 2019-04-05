c50949e6235943639d7c92bae338159c10fcd092
ghh82058 <guanhuih@outlook.com>
2019-04-05 16:00:44 +0800

    fix some lint errors

--- src/app/modules/data-management/data-management.module.ts
+++ src/app/modules/data-management/data-management.module.ts
@@ -1,14 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSnackBarModule,
+  MatFormFieldModule,
+  MatInputModule,
+} from '@angular/material';
 
 import { DataManagementRoutingModule } from './data-management-routing.module';
 import { DataManagementComponent } from './data-management.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { RecordListComponent } from './components/record-list/record-list.component';
+import { DataReviewComponent } from './components/data-review/data-review.component';
 
 @NgModule({
-  declarations: [DataManagementComponent],
+  declarations: [DataManagementComponent, MenuComponent, RecordListComponent, DataReviewComponent],
   imports: [
     CommonModule,
-    DataManagementRoutingModule
+    HttpClientModule,
+    DataManagementRoutingModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule
   ]
 })
 export class DataManagementModule { }
