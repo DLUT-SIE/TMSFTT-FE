1ca46dd9a9a3896d92187c19d77a487676119679
dutlzn <2436013662@qq.com>
2019-04-11 14:37:45 +0800

    管理校内列表提供真实数据. #106

--- src/app/training-event/components/admin-campus-event-list/admin-campus-event-list.component.ts
+++ src/app/training-event/components/admin-campus-event-list/admin-campus-event-list.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { CampusEventResponse } from 'src/app/shared/interfaces/event';
 import { EventService  } from '../../services/event.service';
+import { ProgramService } from '../../../training-program/services/program.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { GenericListComponent } from 'src/app/shared/generics/generic-list/generic-list';
+import { Program } from 'src/app/shared/interfaces/program';
 
 @Component({
   selector: 'app-admin-campus-event-list',
@@ -14,15 +16,17 @@ import { GenericListComponent } from 'src/app/shared/generics/generic-list/gener
 export class AdminCampusEventListComponent extends GenericListComponent<CampusEventResponse> implements OnInit {
 
   programId: number;
+  program: Program;
 
   constructor(
     private readonly eventService: EventService,
+    private readonly programService: ProgramService,
     protected readonly route: ActivatedRoute,
     protected readonly location: Location,
     protected readonly router: Router,
   ) {
     super(route, router, location);
-  }
+  } 
 
   getResults(offset: number, limit: number) {
     const extraParams = new Map();
@@ -33,6 +37,10 @@ export class AdminCampusEventListComponent extends GenericListComponent<CampusEv
   ngOnInit() {
     this.route.queryParams.subscribe(queryParams => {
       this.programId = queryParams.program_id;
+      this.programService.getProgram(Number(this.programId)).subscribe(program=> {
+        this.program = program;
+        console.log(this.program);
+      });
   });
   super.ngOnInit();
   }
