4d85b3c81f6a3cfe9f36cecbda83b554323e083a
Yqzzxlj <yq736677@icloud.com>
2019-04-12 11:04:07 +0800

    add feedback-dialog component.

--- src/app/training-record/training-record.module.ts
+++ src/app/training-record/training-record.module.ts
@@ -8,6 +8,7 @@ import { RecordListComponent } from './components/record-list/record-list.compon
 import { RecordDetailComponent } from './components/record-detail/record-detail.component';
 import { OffCampusEventRecordListComponent } from './components/off-campus-event-record-list/off-campus-event-record-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { FeedbackDialogComponent } from './components/feedback-dialog/feedback-dialog.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +18,15 @@ import { SharedModule } from 'src/app/shared/shared.module';
     RecordListComponent,
     RecordDetailComponent,
     OffCampusEventRecordListComponent,
+    FeedbackDialogComponent,
 
   ],
   imports: [
     TrainingRecordRoutingModule,
     SharedModule,
   ],
+  entryComponents: [
+    FeedbackDialogComponent,
+  ]
 })
 export class TrainingRecordModule { }
