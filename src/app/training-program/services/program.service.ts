9a55ec19bdb69434ff67b4efa5cf0480a0925bca
a8520238 <360238076@qq.com>
2019-04-11 23:29:55 -0700

    修改项目详情 修改表单路由 可以从后台取回表单数据

--- src/app/training-program/services/program.service.ts
+++ src/app/training-program/services/program.service.ts
@@ -5,6 +5,9 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Program } from 'src/app/shared/interfaces/program';
 import { ProgramRequest } from 'src/app/shared/interfaces/program';
+import { ProgramCategory } from 'src/app/shared/interfaces/program-category';
+import { ProgramForm } from 'src/app/shared/interfaces/program-form';
+import { PaginatedResponse } from 'src/app/shared/interfaces/paginated-response';
 
 import { GenericListService } from 'src/app/shared/generics/generic-list-service/generic-list-service';
 import { ListRequest } from 'src/app/shared/interfaces/list-request';
@@ -36,4 +39,15 @@ export class ProgramService extends GenericListService {
     return this.http.post<Program>(
       `${environment.API_URL}/programs/`, req);
   }
+
+  getProgramCategory() {
+    return this.http.get<PaginatedResponse<ProgramCategory>>(
+      `${environment.API_URL}/program-categories/`);
+  }
+
+  getProgramForm() {
+    return this.http.get<PaginatedResponse<ProgramForm>>(
+      `${environment.API_URL}/program-forms/`);
+  }
+
 }
