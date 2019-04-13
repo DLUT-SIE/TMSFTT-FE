33cec64f3cd50e37ffded8e0eee27c4ed8ddeec8
dutlzn <2436013662@qq.com>
2019-04-13 10:34:23 +0800

    修改测试文件

--- src/app/training-event/components/admin-campus-event-list/admin-campus-event-list.component.spec.ts
+++ src/app/training-event/components/admin-campus-event-list/admin-campus-event-list.component.spec.ts
@@ -4,6 +4,8 @@ import { MatProgressSpinnerModule, MatPaginatorModule, MatIconModule } from '@an
 import { Router, ActivatedRoute } from '@angular/router';
 import { of as observableOf, Subject  } from 'rxjs';
 import { CampusEventResponse } from 'src/app/shared/interfaces/event';
+import { Program } from 'src/app/shared/interfaces/program';
+import { ProgramService } from '../../../training-program/services/program.service';
 import { EventService } from '../../services/event.service';
 import { PaginatedResponse } from 'src/app/shared/interfaces/paginated-response';
 import { HAMMER_LOADER } from '@angular/platform-browser';
@@ -14,6 +16,8 @@ describe('AdminCampusEventListComponent', () => {
   let fixture: ComponentFixture<AdminCampusEventListComponent>;
   let getCampusEvents$: Subject<PaginatedResponse<CampusEventResponse>>;
   let getCampusEvents: jasmine.Spy;
+  let getProgram$: Subject<Program>;
+  let getProgram: jasmine.Spy;
   let navigate: jasmine.Spy;
 
   beforeEach(async(() => {
@@ -21,6 +25,10 @@ describe('AdminCampusEventListComponent', () => {
     getCampusEvents$ = new Subject<PaginatedResponse<CampusEventResponse>>();
     getCampusEvents = jasmine.createSpy();
     getCampusEvents.and.returnValue(getCampusEvents$);
+    getProgram$ = new Subject<Program>();
+    getProgram = jasmine.createSpy();
+    getProgram.and.returnValue(getProgram$);
+
     TestBed.configureTestingModule({
       declarations: [
         AdminCampusEventListComponent,
@@ -40,7 +48,7 @@ describe('AdminCampusEventListComponent', () => {
                 get: () => '1',
               },
             }
-          },
+           },
         },
         {
           provide: Location,
@@ -56,6 +64,12 @@ describe('AdminCampusEventListComponent', () => {
           provide: EventService,
           useValue: {
             getCampusEvents: () => getCampusEvents$,
+          }
+        },
+        {
+          provide: ProgramService,
+          useValue: {
+            getProgram: () => getProgram$,
           }
         },
         {
@@ -75,4 +89,32 @@ describe('AdminCampusEventListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load data', () => {
+
+    const dummyProgram: Program = {
+      id: 1,
+      name: 'sender',
+      department: 2,
+      category: 3,
+      department_detail: {
+        id: 2,
+        create_time: '2019-3-4',
+        update_time: '2019-3-6',
+        name: 'test',
+        admins: [],
+      },
+      category_detail: {
+        id: 3,
+        name: 'test',
+      },
+      form: [],
+    };
+    // const result = dummyProgram;
+    getProgram$.next(dummyProgram);
+
+    expect(component.program).toBe(dummyProgram);
+    expect(component.getResults(0, 0)).toBe(getCampusEvents$);
+  });
+
 });
