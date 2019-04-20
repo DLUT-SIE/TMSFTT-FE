c3cff04af23f6cb77599971b3964ea32befea328
Youchen Du <youchen.du@gmail.com>
2019-04-20 17:13:51 +0800

    Add comments for changes.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ import { Environment } from './environment.interface';
 export const environment: Environment = {
   production: true,
   JWT_KEY: 'ACCESS_TOKEN',
+  // TODO(youchen): Replace with production server.
   CAS_LOGIN_URL: 'http://localhost:8000/mock-cas/login/',
   SERVICE_URL: 'http://localhost:8000/auth/login/',
   WHITE_LIST_DOMAINS: ['localhost:8000'],
