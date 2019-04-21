6f591331cd346b9eac9f8ddfe7305459e1e5e3fc
Eyeeco <1911362549@qq.com>
2019-04-21 15:51:24 +0800

    backspace error

--- src/app/permission-management/components/user-management/user-management.component.ts
+++ src/app/permission-management/components/user-management/user-management.component.ts
@@ -68,8 +68,8 @@ export class UserManagementComponent implements OnInit {
         this.department = department;
       }),
       switchMap(() => {
-          return zip(...this.user.groups.map(
-            x => this.groupService.getGroupById(x)));
+        return zip(...this.user.groups.map(
+          x => this.groupService.getGroupById(x)));
       }),
       map((groups: Group[]) => {
         this.groups = groups;
