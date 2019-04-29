783a0dd4406e134ff0c5c45dab04cfe0a51911c5
wangyang <1466738696@qq.com>
2019-04-29 15:14:17 +0800

    add whitespace to meet travis-ci demand.

--- src/app/admin/modules/data-management/components/data-graph/data-graph.component.ts
+++ src/app/admin/modules/data-management/components/data-graph/data-graph.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Graph } from 'src/app/shared/interfaces/graph';
 import { FormBuilder, Validators, ValidatorFn, FormGroup, ValidationErrors} from '@angular/forms';
 
-export const TimeValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
+export const timeValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
     const selectedStartYear = control.get('selectedStartYear');
     const selectedEndYear = control.get('selectedEndYear');
-  
-    return selectedStartYear.value && selectedEndYear.value && selectedStartYear.value > selectedEndYear.value ? { 'TimeValidator': true } : null;
+
+    return selectedStartYear.value && selectedEndYear.value && selectedStartYear.value > selectedEndYear.value ?
+           { 'timeValidator': true } : null;
 };
 
 
@@ -26,51 +27,6 @@ export class DataGraphComponent implements OnInit {
   startYearList: number[];
   endYearList: number[];
 
-  get selectedGraphType(){
-    return this.selectedGraph.get('selectedGraphType');
-  }
-
-  get selectedGroupType(){
-    return this.selectedGraph.get('selectedGroupType');
-  }
-
-  get selectedStartYear(){
-      return this.selectedGraph.get('selectedStartYear');
-  }
-
-  get selectedEndYear(){
-      return this.selectedGraph.get('selectedEndYear');
-  }
-
-  get selectedDepartment(){
-      return this.selectedGraph.get('selectedDepartment');
-  }
-
-  Docheck(){
-    this.selectedGraphType.valueChanges.subscribe(val => {
-        if(val > 2){
-            this.showDepartmentSelector = false;
-        }
-        else{
-            this.showDepartmentSelector = true;
-        }
-        this.selectedGraph.patchValue({'selectedGroupType': null, 'selectedDepartment': '全校'});
-      });
-    this.selectedGroupType.valueChanges.subscribe(val =>{
-        if(val === null)return;
-        if(this.selectedGraphType.value > 2 || val === 0){
-            this.showDepartmentSelector = false;
-            this.selectedGraph.patchValue({'selectedDepartment': '全校'});
-        }
-        else{
-            this.showDepartmentSelector = true;
-        }
-    });
-    this.selectedGraph.statusChanges.subscribe(val=>{
-        if(val === "VALID")this.selectedGraphValues = this.selectedGraph.value;
-    });
-  }
-
   statisticsType : Graph[] = [{id: 0 , name: '教职工人数统计'}, {id: 1, name: '培训人数统计'},
                                {id: 2, name: '专任教师培训覆盖率统计'}, {id: 3, name: '培训学时与工作量统计'}];
   peopleLabelType: Graph[] = [{id: 0, name: '按学院'}, {id: 1, name: '按人员类别'}, {id: 2, name: '按职称'},
@@ -82,26 +38,70 @@ export class DataGraphComponent implements OnInit {
   
   secondType = [this.peopleLabelType, this.trainLabelType, this.trainLabelType, this.trainHourLabelType];
 
-                          
+  get selectedGraphType() {
+    return this.selectedGraph.get('selectedGraphType');
+  }
+
+  get selectedGroupType() {
+    return this.selectedGraph.get('selectedGroupType');
+  }
+
+  get selectedStartYear() {
+      return this.selectedGraph.get('selectedStartYear');
+  }
+
+  get selectedEndYear() {
+      return this.selectedGraph.get('selectedEndYear');
+  }
+
+  get selectedDepartment() {
+      return this.selectedGraph.get('selectedDepartment');
+  }
+
+  Docheck() {
+    this.selectedGraphType.valueChanges.subscribe(val => {
+        if(val > 2) {
+            this.showDepartmentSelector = false;
+        }
+        else {
+            this.showDepartmentSelector = true;
+        }
+        this.selectedGraph.patchValue({'selectedGroupType': null, 'selectedDepartment': '全校'});
+      });
+    this.selectedGroupType.valueChanges.subscribe(val => {
+        if (val === null)return;
+        if (this.selectedGraphType.value > 2 || val === 0) {
+            this.showDepartmentSelector = false;
+            this.selectedGraph.patchValue({'selectedDepartment': '全校'});
+        }
+        else {
+            this.showDepartmentSelector = true;
+        }
+    });
+    this.selectedGraph.statusChanges.subscribe(val => {
+        if (val === 'VALID')this.selectedGraphValues = this.selectedGraph.value;
+    });
+  }
+               
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
       this.currentYear = (new Date()).getFullYear();
       this.startYear = 2014;
       this.showDepartmentSelector = true;
-      this.selectedGraph= this.fb.group({
+      this.selectedGraph = this.fb.group({
         selectedGraphType: [null, Validators.required],
         selectedGroupType: [null, Validators.required],
         selectedStartYear: [this.currentYear, Validators.required],
         selectedEndYear: [this.currentYear, Validators.required],
         selectedDepartment: ['全校']
-        },{ validator: TimeValidator })
+        }, { validator: timeValidator })
       this.selectedGraphValues = null;
-      this. Docheck();
+      this.Docheck();
       this.startYearList = [];
       this.endYearList = [];
-      var i:number;
-      for(i = this.startYear; i <= this.currentYear;i ++){
+      let i:number;
+      for (i = this.startYear; i <= this.currentYear;i ++) {
           this.startYearList.push(i);
           this.endYearList.push(i);
       }
