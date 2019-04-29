bde0e3280860e3c5cd7ea19a5987b93653654762
a8520238 <360238076@qq.com>
2019-04-29 05:14:32 -0700

    删除培训形式 更改培训类别 一些命名优化

--- src/app/admin/modules/program-management/admin-programs/components/admin-program-form/admin-program-form.component.ts
+++ src/app/admin/modules/program-management/admin-programs/components/admin-program-form/admin-program-form.component.ts
@@ -3,14 +3,10 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material';
-import { map } from 'rxjs/operators';
 
 import { Program } from 'src/app/shared/interfaces/program';
 import { ProgramCategory } from 'src/app/shared/interfaces/program-category';
-import { ProgramForm } from 'src/app/shared/interfaces/program-form';
 import { ProgramService} from 'src/app/shared/services/programs/program.service';
-import { ProgramFormService} from 'src/app/shared/services/programs/program-form.service';
-import { ProgramCategoryService} from 'src/app/shared/services/programs/program-category.service';
 import { AuthService, AUTH_SERVICE } from 'src/app/shared/interfaces/auth-service';
 
 /* Create or update a admin-program-form**/
@@ -22,42 +18,28 @@ import { AuthService, AUTH_SERVICE } from 'src/app/shared/interfaces/auth-servic
 export class AdminProgramFormComponent implements OnInit {
 
   programCategories: ProgramCategory[] = [];
-  programForms: ProgramForm[] = [];
   programId: number;
   program: Program;
 
   programForm = this.fb.group({
     name: ['', [Validators.required, Validators.maxLength(50)]],
     categoryId: ['', [Validators.required]],
-    formIds: ['', [Validators.required]],
   });
 
   constructor(
     private readonly fb: FormBuilder,
     private readonly snackBar: MatSnackBar,
     private readonly programService: ProgramService,
-    private readonly programFormService: ProgramFormService,
-    private readonly programCategoryService: ProgramCategoryService,
     private readonly router: Router,
     private readonly route: ActivatedRoute,
     @Inject(AUTH_SERVICE) private readonly authService: AuthService,
   ) { }
 
   ngOnInit() {
-    this.programCategoryService.getProgramCategories({offset: 0, limit: 100}).pipe(
-      map(data => {
-      return data.results;
-    })).subscribe(
+    this.programService.getProgramCategories().subscribe(
       programCategories => {this.programCategories = programCategories;
-      }
-    );
-    this.programFormService.getProgramForms({offset: 0, limit: 100}).pipe(
-      map(data => {
-      return data.results;
-    })).subscribe(
-      programForms => {this.programForms = programForms;
-      }
-    );
+    });
+
     /* istanbul ignore else  */
     if (this.route.snapshot.queryParams.program_id !== undefined) {
       const programId = this.route.snapshot.queryParams.program_id;
@@ -67,7 +49,6 @@ export class AdminProgramFormComponent implements OnInit {
             this.program = program;
             this.name.setValue(this.program.name);
             this.categoryId.setValue(this.program.category);
-            this.formIds.setValue(this.program.form);
         });
       }
   }
@@ -78,20 +59,16 @@ export class AdminProgramFormComponent implements OnInit {
   get categoryId() {
     return this.programForm.get('categoryId');
   }
-  get formIds() {
-    return this.programForm.get('formIds');
-  }
 
   onSubmit() {
     const req: Program = {
       department: this.authService.department,
       category: this.programForm.value.categoryId,
       name: this.programForm.value.name,
-      form: this.programForm.value.formIds,
     };
     this.programService.createProgram(req).subscribe(
       program => {
-        this.router.navigate(['/admin/event-management/programs/events'], { queryParams: {program_id: program.id}});
+        this.router.navigate(['/admin/programs/', program.id]);
       },
       (error: HttpErrorResponse) => {
         let message = error.message;
