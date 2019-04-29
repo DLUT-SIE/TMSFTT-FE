99324750fc66f83aa32231e202c885ca14fd6018
a8520238 <360238076@qq.com>
2019-04-29 04:59:12 -0700

    之前未知原因删掉的补回来

---
+++ src/app/admin/modules/permission-management/components/department-group/department-group.component.spec.ts
@@ -0,0 +1,58 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DepartmentGroupComponent } from './department-group.component';
+import { GroupService } from 'src/app/admin/modules/permission-management/services/group.service';
+import { Department } from 'src/app/shared/interfaces/department';
+import { Subject } from 'rxjs';
+import { Group } from 'src/app/shared/interfaces/group';
+import { PaginatedResponse } from 'src/app/shared/interfaces/paginated-response';
+
+function generatePaginatedGroups(n?: number): PaginatedResponse<Group> {
+  n = n || 5;
+  const group: Group[] = [];
+  for (let i = 0; i < n; i++) {
+    group.push({id: i, name: `${i}`});
+  }
+  const res = { count: n, previous: '', next: '', results: group};
+  return res;
+}
+
+describe('DepartmentGroupComponent', () => {
+  let component: DepartmentGroupComponent;
+  let fixture: ComponentFixture<DepartmentGroupComponent>;
+  let getGroupByDepartmentName$: Subject<{}>;
+
+  beforeEach(async(() => {
+    getGroupByDepartmentName$ = new Subject();
+    TestBed.configureTestingModule({
+      declarations: [ DepartmentGroupComponent ],
+      providers: [
+        {
+          provide: GroupService,
+          useValue: {
+            getGroupByDepartmentName: () => getGroupByDepartmentName$,
+          },
+        },
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DepartmentGroupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should get Groups from department', () => {
+    const n = 5;
+    const department = {id: 1, name: 'name'} as Department;
+    component.departmentSelected = department;
+    getGroupByDepartmentName$.next(generatePaginatedGroups(n));
+
+    expect(component.departmentGroup.length).toBe(n);
+  });
+});
--- src/app/admin/modules/permission-management/components/department-management/department-management.component.ts
+++ src/app/admin/modules/permission-management/components/department-management/department-management.component.ts
@@ -1,28 +1,21 @@
-import { Component } from '@angular/core';
-import { GenericListComponent } from 'src/app/shared/generics/generic-list/generic-list';
+import { Component, OnInit } from '@angular/core';
 import { Department } from 'src/app/shared/interfaces/department';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Location } from '@angular/common';
-import { DepartmentService } from 'src/app/shared/services/department.service';
 
 @Component({
   selector: 'app-department-management',
   templateUrl: './department-management.component.html',
   styleUrls: ['./department-management.component.css']
 })
-export class DepartmentManagementComponent extends GenericListComponent<Department> {
+export class DepartmentManagementComponent implements OnInit {
 
-  constructor(
-    protected readonly route: ActivatedRoute,
-    protected readonly router: Router,
-    protected readonly location: Location,
-    private readonly departmentService: DepartmentService,
-  ) {
-    super(route, router, location);
+  departmentSelected: Department = null;
+
+  constructor() { }
+
+  ngOnInit() {
   }
 
-  getResults(offset: number, limit: number) {
-    return this.departmentService.getDepartments({offset, limit});
+  onchanged(department: Department) {
+    this.departmentSelected = department;
   }
-
 }
