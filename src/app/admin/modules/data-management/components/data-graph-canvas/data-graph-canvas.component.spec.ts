4c9dc2566c1351bcb13b307ad3ad9f8c03ea3747
wangyang <1466738696@qq.com>
2019-05-04 09:34:10 +0800

    add data-graph-canvas test file.

--- src/app/admin/modules/data-management/components/data-graph-canvas/data-graph-canvas.component.spec.ts
+++ src/app/admin/modules/data-management/components/data-graph-canvas/data-graph-canvas.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DataGraphCanvasComponent } from './data-graph-canvas.component';
 import { NgxEchartsModule } from 'ngx-echarts';
+import { StatisticsType } from 'src/app/shared/enums/statistics-type.enum';
+import { DataGraphConfiguration } from 'src/app/shared/interfaces/data-graph-configuration';
 
 describe('DataGraphCanvasComponent', () => {
   let component: DataGraphCanvasComponent;
@@ -23,5 +25,35 @@ describe('DataGraphCanvasComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get null', () => {
+    expect(component.graphParam = null).toBe(null);
+  });
+
+  it('should get a pieGraph', () => {
+    const graphParam: DataGraphConfiguration = {
+      selectedStatisticsType: 0,
+      selectedDepartment: '全校',
+      selectedStartYear: 2019,
+      selectedEndYear: 2019,
+      selectedGroupType: 2
+    };
+    component.graphTypeName = '123';
+    component.isPieGraph = true;
+    component.graphParam = graphParam;
+    expect(component.chartOption).toBe(component.pieChartOption);
+  });
+
+  it('should get a double bar graph', () => {
+    component.isPieGraph = false;
+    component.graphParam = {selectedStatisticsType: StatisticsType.STAFF_STATISTICS} as DataGraphConfiguration;
+    expect(component.chartOption).toBe(component.doubleBarChartOption);
+  });
+
+  it('should get a bar graph', () => {
+    component.isPieGraph = false;
+    component.graphParam = {selectedStatisticsType: StatisticsType.TRAINEE_STATISTICS} as DataGraphConfiguration;
+    expect(component.chartOption).toBe(component.barChartOption);
   });
 });
