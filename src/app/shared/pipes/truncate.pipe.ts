fd39060b774958ba26f96ae0f2b6238806b5b851
Youchen Du <youchen.du@gmail.com>
2019-05-05 17:17:00 +0800

    Remove type any.

--- src/app/shared/pipes/truncate.pipe.ts
+++ src/app/shared/pipes/truncate.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TruncatePipe implements PipeTransform {
 
-  transform(value: string, length?: any): any {
+  transform(value: string, length?: number): string {
     if (!length || value.length <= length) {
       return value;
     }
