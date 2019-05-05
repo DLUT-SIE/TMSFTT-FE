4f25074faa41ca28fa4b532732621caeee66272f
Youchen Du <youchen.du@gmail.com>
2019-05-05 09:53:26 +0800

    Add tests for apihost interceptor.

--- src/app/core/api-host-interceptor/apihost-interceptor.interceptor.spec.ts
+++ src/app/core/api-host-interceptor/apihost-interceptor.interceptor.spec.ts
@@ -1,12 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 
 import { APIHostInterceptor } from './apihost-interceptor.interceptor';
+import { HttpRequest, HttpHandler } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 describe('APIHostInterceptor', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: APIHostInterceptor = TestBed.get(APIHostInterceptor);
-    expect(service).toBeTruthy();
+    const interceptor: APIHostInterceptor = TestBed.get(APIHostInterceptor);
+    expect(interceptor).toBeTruthy();
+  });
+
+  it('should intercept relative urls', () => {
+    const interceptor: APIHostInterceptor = TestBed.get(APIHostInterceptor);
+    const request = new HttpRequest('GET', '/relative/a');
+    const handle = jasmine.createSpy();
+    interceptor.intercept(request, {handle} as HttpHandler);
+
+    expect(handle).toHaveBeenCalled();
+    expect(handle.calls.count()).toEqual(1);
+    expect((handle.calls.argsFor(0)[0] as HttpRequest<any>).url).toEqual(
+      `${environment.API_URL}${request.url}`
+    );
+  });
+
+  it('should skip absolute urls', () => {
+    const interceptor: APIHostInterceptor = TestBed.get(APIHostInterceptor);
+    const request = new HttpRequest('GET', 'https://a.com/relative/a');
+    const handle = jasmine.createSpy();
+    interceptor.intercept(request, {handle} as HttpHandler);
+
+    expect(handle).toHaveBeenCalled();
+    expect(handle.calls.count()).toEqual(1);
+    expect((handle.calls.argsFor(0)[0] as HttpRequest<any>).url).toEqual(
+      request.url
+    );
   });
 });
