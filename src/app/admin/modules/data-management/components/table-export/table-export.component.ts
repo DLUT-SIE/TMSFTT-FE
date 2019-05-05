493e03105de4e44ebf9d5db8846dcc6daf3b3b03
Spground <958340585@qq.com>
2019-05-05 15:02:29 +0800

    修改单测
    修改部分样式文件以及适应主题

--- src/app/admin/modules/data-management/components/table-export/table-export.component.ts
+++ src/app/admin/modules/data-management/components/table-export/table-export.component.ts
@@ -5,9 +5,9 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { WindowService } from 'src/app/shared/services/window.service';
 import { LoggerService } from 'src/app/shared/services/logger.service';
 
-interface Table {
-    id: number,
-    name: string
+export interface Table {
+    id: number;
+    name: string;
 }
 
 @Component({
@@ -16,16 +16,15 @@ interface Table {
     styleUrls: ['./table-export.component.css'],
 })
 export class TableExportComponent implements OnInit {
-    
-    readonly tables_basic: Table[] = [
+    readonly tablesBasic: Table[] = [
         {id: 1, name: '教职工表'},
         {id: 2, name: '专任教师表'}
     ];
 
-    readonly tables_training: Table[] = [
+    readonly tablesTraining: Table[] = [
         {id: 3, name: '培训总体情况表'},
-        {id: 4, name: '专任教师培训覆盖率表'}, 
-        {id: 5, name: '培训学时与工作量表'}
+        {id: 4, name: '专任教师培训覆盖率表'},
+        {id: 5, name: '培训学时与工作量表'},
     ];
 
     constructor(
@@ -39,8 +38,8 @@ export class TableExportComponent implements OnInit {
     ngOnInit() {
     }
 
-    doTableExport() {
-        const url = `/data-management/table-export`;
+    doTableExport(table: Table) {
+        const url = `/data-management/table-export/?id=${table.id}`;
         this.tableExportService.exportTable(url).subscribe(
             data => {
                 this.windowService.open(data['url']);
