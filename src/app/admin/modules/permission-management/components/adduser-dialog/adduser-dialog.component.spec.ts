296c15cd348da4736a72e0001eeaf9df9c6fde4f
Eyeeco <1911362549@qq.com>
2019-05-09 10:29:08 +0800

    CRUD function of userGroup (Round2)

---
+++ src/app/admin/modules/permission-management/components/adduser-dialog/adduser-dialog.component.spec.ts
@@ -0,0 +1,99 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA,
+         MatDialogRef,
+         MatDialogModule,
+         MatInputModule,
+         MatFormFieldModule,
+         MatButtonModule,
+        } from '@angular/material';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { AdduserDialogComponent } from './adduser-dialog.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { User } from 'src/app/shared/interfaces/user';
+import { UserService } from 'src/app/shared/services/user.service';
+import { Subject } from 'rxjs';
+import { PaginatedResponse } from 'src/app/shared/interfaces/paginated-response';
+
+describe('AdduserDialogComponent', () => {
+  let component: AdduserDialogComponent;
+  let fixture: ComponentFixture<AdduserDialogComponent>;
+  let close: jasmine.Spy;
+  let getUserByUsername$: Subject<PaginatedResponse<User>>;
+  beforeEach(async(() => {
+    getUserByUsername$ = new Subject();
+    close = jasmine.createSpy();
+    TestBed.configureTestingModule({
+      declarations: [ AdduserDialogComponent ],
+      imports: [
+        MatDialogModule,
+        MatInputModule,
+        MatFormFieldModule,
+        NoopAnimationsModule,
+        MatButtonModule,
+        FormsModule,
+        HttpClientTestingModule,
+      ],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            username: 'name',
+          }
+        },
+        {
+          provide: UserService,
+          useValue: {
+            getUserByUsername: () => getUserByUsername$,
+          }
+        },
+        {
+          provide: MatDialogRef,
+          useValue: {
+            close,
+          }
+        }
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdduserDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should close when cancle it', () => {
+    component.onNoClick();
+
+    expect(close).toHaveBeenCalled();
+  });
+
+  it('should retrieve User', () => {
+    component.retrieveUser();
+    expect(component.isLoading).toBeTruthy();
+
+    getUserByUsername$.next({ count: 1, previous: '', next: '', results: [{ id: 1, groups: [1, 2]} as User] });
+
+    expect(component.isLoading).toBeFalsy();
+  });
+
+  it('should display error message.', () => {
+    component.username = 'abc';
+    component.retrieveUser();
+    const errorMessage = '系统中无此用户!';
+
+    expect(component.isLoading).toBeTruthy();
+
+    getUserByUsername$.next({count: 0, results: [], previous: '', next: ''});
+
+    expect(component.isLoading).toBeFalsy();
+    expect(component.errorMessage).toEqual(errorMessage);
+  });
+
+});
