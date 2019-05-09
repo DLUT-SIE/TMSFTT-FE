577444e29a5a9e154ddb0617f4bdf84d72994c31
Eyeeco <1911362549@qq.com>
2019-05-09 10:52:06 +0800

    fix test bugs

--- src/app/admin/modules/permission-management/components/adduser-dialog/adduser-dialog.component.ts
+++ src/app/admin/modules/permission-management/components/adduser-dialog/adduser-dialog.component.ts
@@ -1,5 +1,5 @@
-import { Component, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material';
 
 import { User } from 'src/app/shared/interfaces/user';
 import { UserService } from 'src/app/shared/services/user.service';
