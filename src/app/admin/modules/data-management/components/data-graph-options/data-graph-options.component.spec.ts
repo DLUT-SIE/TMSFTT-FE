78ee03f7bb985680fe6931099b5ccad55199b221
wangyang <1466738696@qq.com>
2019-05-09 12:39:09 +0800

    revise data-graph.

---
+++ src/app/admin/modules/data-management/components/data-graph-options/data-graph-options.component.spec.ts
@@ -0,0 +1,163 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ValidationErrors, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatSelectModule } from '@angular/material';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Subject } from 'rxjs';
+
+import { DataGraphOptionsComponent, timeValidator } from './data-graph-options.component';
+import { DataGraphCanvasComponent } from '../data-graph-canvas/data-graph-canvas.component';
+import { CanvasOptionsService } from 'src/app/shared/services/data/canvas-options.service';
+import { OptionType } from 'src/app/shared/interfaces/option-type';
+import { DepartmentService } from 'src/app/shared/services/department.service';
+import { PaginatedResponse } from 'src/app/shared/interfaces/paginated-response';
+import { Department } from 'src/app/shared/interfaces/department';
+
+describe('DataGraphOptionsComponent', () => {
+  let component: DataGraphOptionsComponent;
+  let fixture: ComponentFixture<DataGraphOptionsComponent>;
+  let getOptions$: Subject<OptionType[]>;
+  let getDepartments$: Subject<PaginatedResponse<Department>>;
+  const options: OptionType[] = [{
+    type: 0,
+    name: '教职工人数统计',
+    key: 'STAFF_STATISTICS',
+    subOption: [{
+      type: 0,
+      name: '按学院',
+      key: 'BY_DEPARTMENT'
+    }, {
+      type: 1,
+      name: '按职称',
+      key: 'BY_STAFF_TITLE'
+    }]
+  }, {
+    type: 1,
+    name: '培训学时与工作量统计',
+    key: 'TRAINING_HOURS_WORKLOAD_STATISTICS',
+    subOption: [{
+      type: 0,
+      name: '按总人数',
+      key: 'BY_TOTAL_STAFF_NUM'
+    }]
+  }, {
+    type: 2,
+    name: '专任教师培训覆盖率统计',
+    key: 'FULL_TIME_TEACHER_TRAINED_COVERAGE',
+    subOption: [{
+      type: 0,
+      name: '按学院',
+      key: 'BY_DEPARTMENT'
+    }]
+  }];
+
+  beforeEach(async(() => {
+    getOptions$ = new Subject();
+    getDepartments$ = new Subject();
+    TestBed.configureTestingModule({
+      declarations: [ DataGraphOptionsComponent, DataGraphCanvasComponent ],
+      imports: [
+        MatFormFieldModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+        NgxEchartsModule,
+        BrowserAnimationsModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        {
+          provide: CanvasOptionsService,
+          useValue: {
+            getCanvasOptions: () => getOptions$
+          }
+        },
+        {
+          provide: DepartmentService,
+          useValue: {
+            getDepartments: () => getDepartments$
+          }
+        }
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DataGraphOptionsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    getOptions$.next(options);
+    getDepartments$.next({
+      results: [{id: 1,
+        name: '111'
+      } as Department]
+    } as PaginatedResponse<Department>);
+    const departmentResult = [{
+      id: 0,
+      name: '全校'
+    }, {
+      id: 1,
+      name: '111'
+    }];
+    expect(component).toBeTruthy();
+    expect(component.departmentsList).toEqual(departmentResult as Department[]);
+  });
+
+  it('should get null', () => {
+      const testFormGroup = component.selectedGraph;
+      expect(timeValidator(testFormGroup)).toBe(null);
+
+      testFormGroup.patchValue({selectedStartYear: null, selectedEndYear: 1});
+      expect(timeValidator(testFormGroup)).toBe(null);
+
+      testFormGroup.patchValue({selectedStartYear: 1, selectedEndYear: null});
+      expect(timeValidator(testFormGroup)).toBe(null);
+
+      testFormGroup.patchValue({selectedStartYear: 1, selectedEndYear: 1});
+      expect(timeValidator(testFormGroup)).toBe(null);
+
+      testFormGroup.patchValue({selectedStartYear: 1, selectedEndYear: 3});
+      expect(timeValidator(testFormGroup)).toBe(null);
+
+      testFormGroup.patchValue({selectedStartYear: '', selectedEndYear: ''});
+      expect(timeValidator(testFormGroup)).toBe(null);
+  });
+
+  it('should get a FormGroup ValidationErrors', () => {
+    component.selectedGraph.patchValue({selectedStartYear: 3, selectedEndYear: 1});
+    expect(timeValidator(component.selectedGraph)).toEqual({ timeValidator: true } as ValidationErrors);
+  });
+
+  it('should hide the departmentSelector', () => {
+    getOptions$.next(options);
+    component.selectedGraph.patchValue({selectedStatisticsType: 1});
+    expect(component.selectedGraph.get('selectedGroupType').value).toBe(null);
+    expect(component.showDepartmentSelector).toBeFalsy();
+    expect(component.selectedGraph.get('selectedDepartment').value).toEqual(0);
+
+    component.selectedGraph.patchValue({selectedGroupType: 0});
+    expect(component.showDepartmentSelector).toBeFalsy();
+    expect(component.selectedGraph.get('selectedDepartment').value).toEqual(0);
+  });
+
+  it('should display the departmentSelector', () => {
+    getOptions$.next(options);
+    component.selectedGraph.patchValue({selectedStatisticsType: 0});
+    expect(component.selectedGraph.get('selectedGroupType').value).toBe(null);
+    expect(component.showDepartmentSelector).toBeTruthy();
+    expect(component.selectedGraph.get('selectedDepartment').value).toEqual(0);
+
+    component.selectedGraph.patchValue({
+      selectedGroupType: 0
+    });
+    expect(component.showDepartmentSelector).toBeFalsy();
+    expect(component.selectedGraph.get('selectedDepartment').value).toEqual(0);
+
+    component.selectedGraph.patchValue({selectedGroupType: 1});
+    expect(component.showDepartmentSelector).toBeTruthy();
+  });
+});
--- src/app/admin/modules/data-management/components/data-graph/data-graph.component.ts
+++ src/app/admin/modules/data-management/components/data-graph/data-graph.component.ts
@@ -1,20 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, ValidatorFn, FormGroup, ValidationErrors} from '@angular/forms';
-import { OptionType } from 'src/app/shared/interfaces/option-type';
+import { DataGraphOption } from 'src/app/shared/interfaces/data-graph-option'
 import { DataGraphConfiguration } from 'src/app/shared/interfaces/data-graph-configuration';
-import { Department } from 'src/app/shared/interfaces/department';
-import { CanvasOptionsService } from 'src/app/shared/services/data/canvas-options.service';
-import { DepartmentService } from 'src/app/shared/services/department.service';
-import { ListRequest } from 'src/app/shared/interfaces/list-request';
-
-export const timeValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
-    const selectedStartYear = control.get('selectedStartYear');
-    const selectedEndYear = control.get('selectedEndYear');
-
-    return selectedStartYear.value && selectedEndYear.value && selectedStartYear.value > selectedEndYear.value ?
-           { timeValidator: true } : null;
-};
-const WHOLE_SCHOOL = 0;
 
 @Component({
   selector: 'app-data-graph',
@@ -23,86 +9,20 @@ const WHOLE_SCHOOL = 0;
 })
 export class DataGraphComponent implements OnInit {
 
-  selectedGraphValues: DataGraphConfiguration = null;
-  selectedGraph: FormGroup;
-  showDepartmentSelector = true;
-  yearList: number[] = [];
+  graphParam: DataGraphConfiguration;
+  hidePieGraph: boolean;
+  graphTypeName: string;
+  selectedDepartmentName: string;
 
-  departmentsList: Department[] = [{id: 0, name: '全校'} as Department];
-  statisticsType: OptionType[];
-
-  get graphTypeName() {
-    return this.selectedGraphValues === null ? '' : this.statisticsType[
-      this.selectedGraphValues.selectedStatisticsType].name;
-  }
-
-  get isCoverageGraph() {
-    return this.selectedGraphValues === null ? false :
-    this.statisticsType[this.selectedGraphValues.selectedStatisticsType].key ===
-      'FULL_TIME_TEACHER_TRAINED_COVERAGE';
-  }
-
-  get selectedDepartmentName() {
-    return this.selectedGraphValues === null ? '' : this.departmentsList[
-      this.selectedGraphValues.selectedDepartment].name;
-  }
-
-  SelectedParamChangingCheck() {
-    this.selectedGraph.get('selectedStatisticsType').valueChanges.subscribe(val => {
-      if (val === null) {
-        return;
-      }
-      this.showDepartmentSelector = this.statisticsType[val]
-        .key === 'TRAINING_HOURS_WORKLOAD_STATISTICS' ? false : true;
-      this.selectedGraph.patchValue({
-        selectedGroupType: null,
-        selectedDepartment: WHOLE_SCHOOL
-      });
-    });
-    this.selectedGraph.get('selectedGroupType').valueChanges.subscribe(val => {
-      if (val === null) {
-        return;
-      }
-      const first = this.selectedGraph.get('selectedStatisticsType').value;
-      const second = this.selectedGraph.get('selectedGroupType').value;
-      if (this.statisticsType[first].key === 'TRAINING_HOURS_WORKLOAD_STATISTICS' ||
-          this.statisticsType[first].subOption[second].key === 'BY_DEPARTMENT') {
-          this.showDepartmentSelector = false;
-          this.selectedGraph.patchValue({
-            selectedDepartment: WHOLE_SCHOOL
-          });
-      } else {
-          this.showDepartmentSelector = true;
-      }
-    });
-    this.selectedGraph.statusChanges.subscribe(val => {
-        if (val === 'VALID') {
-          this.selectedGraphValues = this.selectedGraph.value;
-        }
-    });
-  }
-
-  constructor(
-    private fb: FormBuilder,
-    private readonly departmentService: DepartmentService,
-    private readonly canvasOptionsService: CanvasOptionsService
-    ) { }
+  constructor() { }
 
   ngOnInit() {
-      for (let i = 2016; i <= (new Date()).getFullYear(); i++) {
-          this.yearList.push(i);
-      }
-      this.selectedGraph = this.fb.group({
-        selectedStatisticsType: [null, Validators.required],
-        selectedGroupType: [null, Validators.required],
-        selectedStartYear: [this.yearList[this.yearList.length - 1], Validators.required],
-        selectedEndYear: [this.yearList[this.yearList.length - 1], Validators.required],
-        selectedDepartment: [WHOLE_SCHOOL]
-        }, { validator: timeValidator });
-      this.departmentService.getDepartments({offset: 0, limit: 100} as ListRequest)
-          .subscribe(departments => this.departmentsList = this.departmentsList.concat(departments.results));
-      this.canvasOptionsService.getCanvasOptions()
-          .subscribe(options => this.statisticsType = options);
-      this.SelectedParamChangingCheck();
+  }
+
+  getOptions(options: DataGraphOption) {
+    this.graphParam = options.option;
+    this.hidePieGraph = options.isCoverageGraph;
+    this.graphTypeName = options.graphTypeName;
+    this.selectedDepartmentName = options.selectedDepartmentName;
   }
 }
