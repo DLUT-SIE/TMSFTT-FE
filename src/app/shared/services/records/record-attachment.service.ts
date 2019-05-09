bbfe6530a4eadda06a557c764b5b282a8c8771eb
Yqzzxlj <yq736677@icloud.com>
2019-05-09 11:16:27 +0800

    Modify the getAttachment and getContent to use list method

--- src/app/shared/services/records/record-attachment.service.ts
+++ src/app/shared/services/records/record-attachment.service.ts
@@ -3,17 +3,20 @@ import { HttpClient } from '@angular/common/http';
 import { of as observableOf } from 'rxjs';
 
 import { RecordAttachment } from 'src/app/shared/interfaces/record-attachment';
+import { GenericListService } from '../../generics/generic-list-service/generic-list-service';
 import { PaginatedResponse } from '../../interfaces/paginated-response';
 
 /** Export services for RecordAttachment. */
 @Injectable({
   providedIn: 'root'
 })
-export class RecordAttachmentService {
+export class RecordAttachmentService extends GenericListService {
 
   constructor(
-    private readonly http: HttpClient,
-  ) { }
+    protected readonly http: HttpClient,
+  ) {
+    super(http);
+  }
 
   /** Get single attachment. */
   getRecordAttachment(id: number) {
@@ -25,11 +28,10 @@ export class RecordAttachmentService {
   /** Get multiple attachments. */
   getRecordAttachments(ids: number[]) {
     if (ids.length === 0) return observableOf({count: 0, next: '', previous: '', results: []});
+    const extraParams = new Map<string, string>();
+    extraParams.set('id__in', ids.toString());
 
-    const queryParams = 'id__in=' + encodeURIComponent(ids.toString());
-
-    return this.http.get<PaginatedResponse<RecordAttachment>>(
-      `/record-attachments/?${queryParams}`);
+    return this.list<PaginatedResponse<RecordAttachment>>('record-attachments', {extraParams});
   }
 
   deleteRecordAttachment(id: number) {
