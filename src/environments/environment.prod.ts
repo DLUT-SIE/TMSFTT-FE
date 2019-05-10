34d8e7e2ccd9a8e476fca512de1ae605275551ac
Youchen Du <youchen.du@gmail.com>
2019-05-10 13:29:32 +0800

    Update prod settings.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,10 +4,10 @@ export const environment: Environment = {
   production: true,
   JWT_KEY: 'ACCESS_TOKEN',
   // TODO(youchen): Replace with production server.
-  CAS_LOGIN_URL: 'http://localhost:8000/mock-cas/login/',
-  SERVICE_URL: 'http://localhost:8000/auth/login/',
-  WHITE_LIST_DOMAINS: ['localhost:8000'],
-  API_URL: 'http://localhost:8000/api',
+  CAS_LOGIN_URL: 'https://tmsftt.local/mock-cas/login/',
+  SERVICE_URL: 'https://tmsftt.local/auth/login/',
+  WHITE_LIST_DOMAINS: [],
+  API_URL: '/api',
   PAGINATION_SIZE: 10,
   REFRESH_INTERVAL: 30 * 1000,
 };
