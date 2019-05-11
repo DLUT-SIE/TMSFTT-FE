e9d416c7c6a8013c798a1bee849ff75882b859cd
a8520238 <360238076@qq.com>
2019-05-11 06:27:28 -0700

    add view program according to department

--- src/app/admin/modules/program-management/admin-programs/components/admin-program-list/admin-program-list.component.spec.ts
+++ src/app/admin/modules/program-management/admin-programs/components/admin-program-list/admin-program-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HAMMER_LOADER } from '@angular/platform-browser';
-import { MatProgressSpinnerModule, MatPaginatorModule, MatIconModule } from '@angular/material';
+import { MatProgressSpinnerModule, MatPaginatorModule, MatIconModule, MatSelectModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
@@ -10,20 +11,38 @@ import { ProgramService } from 'src/app/shared/services/programs/program.service
 import { AdminProgramListComponent } from './admin-program-list.component';
 import { AUTH_SERVICE } from 'src/app/shared/interfaces/auth-service';
 import { PaginatedResponse } from 'src/app/shared/interfaces/paginated-response';
+import { DepartmentService } from 'src/app/shared/services/department.service';
+import { Department } from 'src/app/shared/interfaces/department';
 
 describe('AdminProgramListComponent', () => {
   let component: AdminProgramListComponent;
   let fixture: ComponentFixture<AdminProgramListComponent>;
   let getPrograms$: Subject<PaginatedResponse<Program>>;
+  let getDepartments$: Subject<PaginatedResponse<Department>>;
   let navigate: jasmine.Spy;
   let getPrograms: jasmine.Spy;
-
+  let getDepartments: jasmine.Spy;
   const dummyProgram: Program = {
     id: 1,
     name: 'sender',
-    department: 1,
+    department: 4,
     category: 3,
     form: [],
+  };
+  const dummyDepartment: Department = {
+    id: 4,
+    name: '大连理工大学',
+    admins: [],
+    users: [],
+    super_department: null,
+  };
+
+  const dummySuperDepartment: Department = {
+    id: 2,
+    name: '凌水主校区',
+    admins: [],
+    users: [],
+    super_department: 4,
   };
 
   beforeEach(async(() => {
@@ -31,6 +50,9 @@ describe('AdminProgramListComponent', () => {
     getPrograms$ = new Subject<PaginatedResponse<Program>>();
     getPrograms = jasmine.createSpy();
     getPrograms.and.returnValue(getPrograms$);
+    getDepartments$ = new Subject<PaginatedResponse<Department>>();
+    getDepartments = jasmine.createSpy();
+    getDepartments.and.returnValue(getDepartments$);
     TestBed.configureTestingModule({
       declarations: [
         AdminProgramListComponent,
@@ -38,14 +60,17 @@ describe('AdminProgramListComponent', () => {
       imports: [
         MatIconModule,
         MatProgressSpinnerModule,
+        MatSelectModule,
         MatPaginatorModule,
         HttpClientTestingModule,
+        BrowserAnimationsModule,
       ],
       providers: [
         {
           provide: AUTH_SERVICE,
           useValue: {
             userID: 1,
+            isSchoolAdmin: true,
           },
         },
         {
@@ -65,11 +90,18 @@ describe('AdminProgramListComponent', () => {
           }
         },
         {
+          provide: DepartmentService,
+          useValue: {
+            getDepartments,
+          }
+        },
+        {
           provide: HAMMER_LOADER,
           useValue: () => new Promise(() => { }),
         },
       ]
     }).compileComponents();
+
   }));
 
   beforeEach(() => {
@@ -82,16 +114,24 @@ describe('AdminProgramListComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should load data', () => {
+  it('should load programs', () => {
     const count = 100;
-    getPrograms$.next({ count,  next: '', previous: '', results: [dummyProgram]});
+    component.loadProgramsBelongToDepartment(dummyDepartment.id);
+    getPrograms$.next({ count,  next: '', previous: '', results: [dummyProgram] });
 
-    expect(component.isLoadingResults).toBeFalsy();
     expect(component.programs.length).toEqual(1);
   });
 
+  it('should load departments', () => {
+    const count = 100;
+    getDepartments$.next({ count,  next: '', previous: '', results: [dummyDepartment, dummySuperDepartment] });
+
+    expect(component.isLoadingResults).toBeFalsy();
+    expect(component.departments.length).toEqual(1);
+  });
+
   it('should empty data if an error encountered.', () => {
-    getPrograms$.error('error');
+    getDepartments$.error('error');
 
     expect(component.isLoadingResults).toBeFalsy();
     expect(component.programs).toEqual([]);
@@ -102,5 +142,73 @@ describe('AdminProgramListComponent', () => {
 
     expect(navigate).toHaveBeenCalledWith(
       ['../../events'], { queryParams: { program_id: dummyProgram.id }, relativeTo: {} });
+  });
+});
+
+describe('NotSchoolAdminAdminProgramListComponent', () => {
+  let component: AdminProgramListComponent;
+  let fixture: ComponentFixture<AdminProgramListComponent>;
+  let navigate: jasmine.Spy;
+  let getPrograms$: Subject<PaginatedResponse<Program>>;
+  let getPrograms: jasmine.Spy;
+
+  beforeEach(async(() => {
+    navigate = jasmine.createSpy();
+    getPrograms$ = new Subject<PaginatedResponse<Program>>();
+    getPrograms = jasmine.createSpy();
+    getPrograms.and.returnValue(getPrograms$);
+    TestBed.configureTestingModule({
+      declarations: [
+        AdminProgramListComponent,
+      ],
+      imports: [
+        MatIconModule,
+        MatProgressSpinnerModule,
+        MatSelectModule,
+        MatPaginatorModule,
+        HttpClientTestingModule,
+        BrowserAnimationsModule,
+      ],
+      providers: [
+        {
+          provide: AUTH_SERVICE,
+          useValue: {
+            userID: 1,
+            isSchoolAdmin: false,
+          },
+        },
+        {
+          provide: Router,
+          useValue: {
+            navigate,
+          }
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+        {
+          provide: ProgramService,
+          useValue: {
+            getPrograms,
+          }
+        },
+        {
+          provide: HAMMER_LOADER,
+          useValue: () => new Promise(() => { }),
+        },
+      ]
+    }).compileComponents();
+
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminProgramListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  it('should convert to loadProgramsBelongToDepartment', () => {
+
+    expect(component.isLoadingResults).toBeFalsy();
   });
 });
--- src/app/admin/modules/program-management/admin-programs/components/admin-program-list/admin-program-list.component.ts
+++ src/app/admin/modules/program-management/admin-programs/components/admin-program-list/admin-program-list.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { map, catchError } from 'rxjs/operators';
+import { tap, map, catchError } from 'rxjs/operators';
 import { of as observableOf } from 'rxjs';
 
 import { Program } from 'src/app/shared/interfaces/program';
 import { ProgramService } from 'src/app/shared/services/programs/program.service';
+import { DepartmentService } from 'src/app/shared/services/department.service';
 import { Department } from 'src/app/shared/interfaces/department';
+import { AuthService, AUTH_SERVICE } from 'src/app/shared/interfaces/auth-service';
 
 @Component({
   selector: 'app-admin-program-list',
@@ -19,32 +21,60 @@ export class AdminProgramListComponent implements OnInit {
   departments: Department[] = [];
   /** The total number of programs. */
   isLoadingResults = true;
-
+  isSchoolAdmin = this.authService.isSchoolAdmin;
   constructor(
     private readonly route: ActivatedRoute,
     private readonly router: Router,
     private readonly programService: ProgramService,
+    private readonly departmentService: DepartmentService,
+    @Inject(AUTH_SERVICE) private readonly authService: AuthService,
   ) { }
 
   ngOnInit() {
-    this.programService.getPrograms({offset: 0, limit: 100}).pipe(
-      map(data => {
-        this.isLoadingResults = false;
-        this.programs = data.results;
-        return this.programs;
-      }),
-      catchError((err) => {
-        this.isLoadingResults = false;
-        return observableOf([]);
-      })).subscribe(
-      data => {
-        this.programs = data;
+    console.log(this.isSchoolAdmin);
+    const department = this.authService.department;
+    let departments: Department[] = [];
+    let secondId;
+    if (this.isSchoolAdmin) {
+      this.departmentService.getDepartments({offset: 0, limit: 200}).pipe(
+        map(data => {
+          departments = data.results;
+          return departments;
+        }),
+        tap(data => {
+          for (let i = 0; i < data.length; i++) {
+            if (data[i].name === '凌水主校区') {
+              secondId = data[i].id;
+          }}
+          for (let i = 0; i < data.length; i++) {
+            if (data[i].super_department === secondId || data[i].name === '大连理工大学') {
+              this.departments.push(data[i]);
+          }}
+        }),
+        catchError((err) => {
+          this.isLoadingResults = false;
+          return observableOf([]);
+        })
+      ).subscribe(data => {
+          this.isLoadingResults = false;
       });
-  }
+    } else {
+      this.loadProgramsBelongToDepartment(department);
+      this.isLoadingResults = false;
+    }
+}
 
   navigateToRelatedEvents(row: Program) {
     this.router.navigate(['../../events'], { queryParams: {program_id: row.id}, relativeTo: this.route});
   }
 
+  loadProgramsBelongToDepartment(department: number) {
+    const extraParams = new Map();
+    extraParams.set('department', department);
+    this.programService.getPrograms({offset: 0, limit: 200, extraParams}).subscribe(
+      data => {
+        this.programs = data.results;
+      });
+    }
 }
 
