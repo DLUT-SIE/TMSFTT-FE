f9cd1571c8d998bf94c1fb563f045a629c658947
Youchen Du <youchen.du@gmail.com>
2019-05-14 10:20:38 +0800

    Hide blocks if no data.

--- src/app/shared/pipes/truncate.pipe.spec.ts
+++ src/app/shared/pipes/truncate.pipe.spec.ts
@@ -14,6 +14,14 @@ describe('TruncatePipe', () => {
     expect(value).toEqual('abccc');
   });
 
+  it('should return value if value is null', () => {
+    const pipe = new TruncatePipe();
+
+    const value = pipe.transform(null);
+
+    expect(value).toBeNull();
+  });
+
   it('should not truncate if length does not exceed', () => {
     const pipe = new TruncatePipe();
 
