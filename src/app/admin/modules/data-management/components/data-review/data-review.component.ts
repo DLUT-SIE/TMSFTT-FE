6a1ccc940382ac205fc9f096f07afad8478071e0
ghh82058 <guanhuih@outlook.com>
2019-05-20 08:52:27 +0800

    Navigate to record list after reviewing.

--- src/app/admin/modules/data-management/components/data-review/data-review.component.ts
+++ src/app/admin/modules/data-management/components/data-review/data-review.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Location } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
 import { HttpErrorResponse } from '@angular/common/http';
 
 import { Record } from 'src/app/shared/interfaces/record';
 import { RecordService } from 'src/app/shared/services/records/record.service';
 import { AuthService, AUTH_SERVICE } from 'src/app/shared/interfaces/auth-service';
+import { RecordStatus } from 'src/app/shared/enums/record-status.enum';
 
 /** Display a record review page in detail. */
 @Component({
@@ -17,9 +18,11 @@ import { AuthService, AUTH_SERVICE } from 'src/app/shared/interfaces/auth-servic
 export class DataReviewComponent implements OnInit {
   /** The data to be displayed. */
   record: Record;
+  isAllowed: Boolean;
 
   constructor(
     protected readonly route: ActivatedRoute,
+    protected readonly router: Router,
     readonly location: Location,
     protected readonly recordService: RecordService,
     protected readonly snackBar: MatSnackBar,
@@ -31,6 +34,9 @@ export class DataReviewComponent implements OnInit {
     this.recordService.updateRecordStatus(this.record.id, isApproved, isDepartmentAdmin)
     .subscribe(() => {
       this.snackBar.open('操作成功！', '关闭');
+      this.router.navigate(['..'], {
+        relativeTo: this.route,
+      });
       },
       (error: HttpErrorResponse) => {
         let message = error.message;
@@ -46,6 +52,9 @@ export class DataReviewComponent implements OnInit {
     this.recordService.closeRecord(this.record.id)
     .subscribe(() => {
       this.snackBar.open('操作成功！', '关闭');
+      this.router.navigate(['..'], {
+        relativeTo: this.route,
+      });
       },
       (error: HttpErrorResponse) => {
         let message = error.message;
@@ -60,6 +69,11 @@ export class DataReviewComponent implements OnInit {
   ngOnInit() {
     this.route.data.subscribe((data: { record: Record}) => {
       this.record = data.record;
+      const recordStatus = data.record.status;
+      this.isAllowed = (!(recordStatus == RecordStatus.STATUS_CLOSED)) && (
+        (this.authService.isDepartmentAdmin && (recordStatus == RecordStatus.STATUS_SUBMITTED)) ||
+        (this.authService.isSchoolAdmin && (recordStatus == RecordStatus.STATUS_FACULTY_ADMIN_REVIEWED)));
+      console.log(this.isAllowed);
     });
   }
 
