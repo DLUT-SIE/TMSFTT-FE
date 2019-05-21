2a68fb2e5f918219d202a091d6191d57fd6e0f0c
a8520238 <360238076@qq.com>
2019-05-20 20:37:24 -0700

    change program form first

--- src/app/admin/modules/program-management/admin-programs/components/admin-program-form/admin-program-form.component.ts
+++ src/app/admin/modules/program-management/admin-programs/components/admin-program-form/admin-program-form.component.ts
@@ -9,6 +9,7 @@ import { Program } from 'src/app/shared/interfaces/program';
 import { ProgramCategory } from 'src/app/shared/interfaces/program-category';
 import { ProgramService} from 'src/app/shared/services/programs/program.service';
 import { AuthService, AUTH_SERVICE } from 'src/app/shared/interfaces/auth-service';
+import { Observable } from 'rxjs';
 
 /* Create or update a admin-program-form**/
 @Component({
@@ -21,6 +22,7 @@ export class AdminProgramFormComponent implements OnInit {
   programCategories: ProgramCategory[] = [];
   programId: number;
   program: Program;
+  isUpdateMode = false;
 
   programForm = this.fb.group({
     name: ['', [Validators.required, Validators.maxLength(50)]],
@@ -45,6 +47,7 @@ export class AdminProgramFormComponent implements OnInit {
     /* istanbul ignore else  */
     if (this.route.snapshot.queryParams.program_id !== undefined) {
       const programId = this.route.snapshot.queryParams.program_id;
+      this.isUpdateMode = true;
       this.programService.getProgram(Number(programId)).
         subscribe(
           program => {
@@ -64,11 +67,15 @@ export class AdminProgramFormComponent implements OnInit {
 
   onSubmit() {
     const req: Program = {
+      id: this.isUpdateMode? this.program.id: undefined,
       department: this.authService.department,
       category: this.programForm.value.categoryId,
       name: this.programForm.value.name,
     };
-    this.programService.createProgram(req).subscribe(
+    const targetProgram: Observable<Program> = this.isUpdateMode?
+                                            this.programService.updateProgram(req) :
+                                            this.programService.createProgram(req);
+    targetProgram.subscribe(
       program => {
         this.router.navigate(['/admin/programs/', program.id]);
       },
