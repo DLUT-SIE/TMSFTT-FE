150fd34fefe303f6679f40b38a796632b1b8e388
dutlzn <2436013662@qq.com>
2019-05-23 18:38:21 +0800

    修改role读取方式.#263

--- src/app/shared/interfaces/coefficients.ts
+++
@@ -1,9 +0,0 @@
-interface Coefficient {
-    coefficient: number;
-    hours_option: number;
-    workload_option: number;
-}
-export interface Coefficients {
-    参与: Coefficient;
-    专家: Coefficient;
-}
