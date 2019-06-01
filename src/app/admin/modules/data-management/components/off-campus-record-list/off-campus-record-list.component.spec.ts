0b9f52a0a303408ba93bb4378cd70c3ebe3567a1
ghh82058 <guanhuih@outlook.com>
2019-06-01 20:36:09 +0800

    Modify the review list component name.

--- src/app/admin/modules/data-management/components/off-campus-record-list/off-campus-record-list.component.spec.ts
+++ src/app/admin/modules/data-management/components/off-campus-record-list/off-campus-record-list.component.spec.ts
@@ -2,16 +2,16 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { RecordListComponent } from './record-list.component';
+import { OffCampusRecordListComponent } from './off-campus-record-list.component';
 import { AppSharedRecordListDirectiveStub } from 'src/testing/app-shared-record-list-stub';
-describe('RecordListComponent', () => {
-  let component: RecordListComponent;
-  let fixture: ComponentFixture<RecordListComponent>;
+describe('OffCampusRecordListComponent', () => {
+  let component: OffCampusRecordListComponent;
+  let fixture: ComponentFixture<OffCampusRecordListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        RecordListComponent,
+        OffCampusRecordListComponent,
         AppSharedRecordListDirectiveStub,
       ],
       providers: [
@@ -39,7 +39,7 @@ describe('RecordListComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RecordListComponent);
+    fixture = TestBed.createComponent(OffCampusRecordListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
