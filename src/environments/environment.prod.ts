3472cbd635f790f1e409dce43d99bc49fe2d1140
Youchen Du <youchen.du@gmail.com>
2019-06-04 09:17:54 +0800

    Adapt prod env.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,8 @@ import { Environment } from './environment.interface';
 export const environment: Environment = {
   production: true,
   JWT_KEY: 'ACCESS_TOKEN',
-  // TODO(youchen): Replace with production server.
-  CAS_LOGIN_URL: 'https://test.feingst.com/mock-cas/login/',
-  SERVICE_URL: 'https://test.feingst.com/auth/login/',
+  CAS_LOGIN_URL: 'https://sso.dlut.edu.cn/cas/login/',
+  SERVICE_URL: 'https://ttms.dlut.edu.cn/',
   WHITE_LIST_DOMAINS: [],
   API_URL: '/api',
   PAGINATION_SIZE: 10,
