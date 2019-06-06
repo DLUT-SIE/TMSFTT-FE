69276cbb0cb8cb1e140acb28156de32fbbf146fd
Youchen Du <youchen.du@gmail.com>
2019-06-06 14:56:26 +0800

    Update CAS_LOGIN_URL and SERVICE_URL.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ import { Environment } from './environment.interface';
 export const environment: Environment = {
   production: true,
   JWT_KEY: 'ACCESS_TOKEN',
-  CAS_LOGIN_URL: 'https://sso.dlut.edu.cn/cas/login/',
-  SERVICE_URL: 'https://ctfdpeixun.dlut.edu.cn/',
+  CAS_LOGIN_URL: 'https://sso.dlut.edu.cn/cas/login',
+  SERVICE_URL: 'https://ctfdpeixun.dlut.edu.cn/auth/login/',
   WHITE_LIST_DOMAINS: [],
   API_URL: '/api',
   PAGINATION_SIZE: 10,
