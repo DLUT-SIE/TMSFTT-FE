deb36b38cd553b9f31ffc6122f1ec8e865f4f5e5
Yqzzxlj <yq736677@icloud.com>
2019-06-07 17:41:07 +0800

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ import { Environment } from './environment.interface';
 export const environment: Environment = {
   production: true,
   JWT_KEY: 'ACCESS_TOKEN',
-  CAS_LOGIN_URL: 'http://127.0.0.1:8000/mock-cas/login/',
-  SERVICE_URL: 'http://127.0.0.1:8000/auth/login/',
-  WHITE_LIST_DOMAINS: ['127.0.0.1:8000',],
+  CAS_LOGIN_URL: 'http://127.0.0.1:80/mock-cas/login/',
+  SERVICE_URL: 'http://127.0.0.1:80/auth/login/',
+  WHITE_LIST_DOMAINS: ['127.0.0.1:80',],
   API_URL: '/api',
   PAGINATION_SIZE: 10,
   REFRESH_INTERVAL: 30 * 1000,
