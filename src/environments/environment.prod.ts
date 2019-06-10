1c8b23c936c1328e88a3fd4b125410907a84d0c5
Youchen Du <youchen.du@gmail.com>
2019-06-10 15:06:05 +0800

    Change SERVICE_URL to HTTP.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment: Environment = {
   production: true,
   JWT_KEY: 'ACCESS_TOKEN',
   CAS_LOGIN_URL: 'https://sso.dlut.edu.cn/cas/login',
-  SERVICE_URL: 'https://ctfdpeixun.dlut.edu.cn/auth/login/',
+  SERVICE_URL: 'http://ctfdpeixun.dlut.edu.cn/auth/login/',
   WHITE_LIST_DOMAINS: [],
   API_URL: '/api',
   PAGINATION_SIZE: 10,
