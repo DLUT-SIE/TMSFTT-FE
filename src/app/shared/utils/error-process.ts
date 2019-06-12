3dfc086f6b463efa6a70459660e558497115b549
Yqzzxlj <yq736677@icloud.com>
2019-06-12 15:11:47 +0800

    fix error dispaly

--- src/app/shared/utils/error-process.ts
+++ src/app/shared/utils/error-process.ts
@@ -1,20 +1,26 @@
 import { HttpErrorResponse } from '@angular/common/http';
 
+function extractMessageFromJSON(obj: any) {
+    if (Array.isArray(obj)) {
+        return obj[0];
+    }
+    let message = '';
+    for (const key of Object.keys(obj)) {
+        if (key === 'detail' || key === 'non_field_errors') {
+            message += '失败原因： ' + extractMessageFromJSON(obj[key]);
+            continue;
+        }
+        message += key + ': ' + extractMessageFromJSON(obj[key]);
+    }
+    return message;
+}
 /** Process http error */
 export function errorProcess(error: HttpErrorResponse) {
     let message = error.message;
-    if (error.error) {
-        message = '';
-        for (const key of Object.keys(error.error)) {
-            if (message !== '') {
-                message += '\n';
-            }
-            if (key === 'detail' || key === 'non_field_errors') {
-                message += '失败原因： ' + error.error[key];
-                continue;
-            }
-            message += key + ': ' + error.error[key];
-        }
+    if (error.status < 500 && error.error) {
+        message = extractMessageFromJSON(error.error);
+    } else if (error.status >= 500) {
+        message = '请求失败： 服务器错误';
     }
     return message;
 }
