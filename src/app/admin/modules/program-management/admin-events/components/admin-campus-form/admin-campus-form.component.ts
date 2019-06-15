fb17fb3e4273e27c0e281a5443dd08d2cbc99dbb
dutlzn <2436013662@qq.com>
2019-06-15 13:33:15 +0800

    modify sumbit button display when create or update

--- src/app/admin/modules/program-management/admin-events/components/admin-campus-form/admin-campus-form.component.ts
+++ src/app/admin/modules/program-management/admin-events/components/admin-campus-form/admin-campus-form.component.ts
@@ -58,6 +58,7 @@ export class AdminCampusFormComponent implements OnInit {
   programId: number;
   isUpdateMode = false;
   event: CampusEvent;
+  submitDisabled = false;
   eventForm = this.fb.group({
     name: ['', [Validators.required, Validators.maxLength(50)]],
     time: ['', [Validators.required, Validators.maxLength(30)]],
@@ -195,6 +196,7 @@ export class AdminCampusFormComponent implements OnInit {
   }
 
   onSubmit() {
+    this.submitDisabled = true;
     const req: CampusEvent = {
       id: this.isUpdateMode ? this.event.id : undefined,
       program: this.programId,
@@ -213,10 +215,12 @@ export class AdminCampusFormComponent implements OnInit {
     targetEvent.subscribe(
       event => {
         this.router.navigate(['admin/programs', this.programId, 'events', event.id]);
+        this.submitDisabled = false;
       },
       (error: HttpErrorResponse) => {
         const message = errorProcess(error);
         this.snackBar.open(message, '关闭');
+        this.submitDisabled = false;
       });
   }
 
