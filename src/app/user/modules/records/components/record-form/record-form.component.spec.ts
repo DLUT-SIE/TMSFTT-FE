82a11b68ef41ff8d64c7cf816ee5c048a1a92a63
Yqzzxlj <yq736677@icloud.com>
2019-07-02 10:01:27 +0800

    提交校外培训记录转圈圈

--- src/app/user/modules/records/components/record-form/record-form.component.spec.ts
+++ src/app/user/modules/records/components/record-form/record-form.component.spec.ts
@@ -14,6 +14,7 @@ import {
   MatNativeDateModule,
   MatSelectModule,
   MatSnackBar,
+  MatProgressSpinnerModule,
   MatAutocompleteSelectedEvent,
 } from '@angular/material';
 import { By } from '@angular/platform-browser';
@@ -97,6 +98,7 @@ describe('RecordFormComponent', () => {
         MatFormFieldModule,
         MatNativeDateModule,
         MatSelectModule,
+        MatProgressSpinnerModule,
       ],
       providers: [
         {
